1a3c1a16d00f6e99eae48efe42606efb5752f211
marverix <2142811+marverix@users.noreply.github.com>
2022-03-15 16:33:10 +0100

    First commit

---
+++ classicpress/wp-config.template.php
@@ -0,0 +1,25 @@
+<?php
+define('DB_NAME', 'CP_DB_NAME');
+define('DB_USER', 'CP_DB_USER');
+define('DB_PASSWORD', 'CP_DB_PASSWORD');
+define('DB_HOST', 'CP_DB_HOST');
+define('DB_CHARSET', 'CP_DB_CHARSET');
+define('DB_COLLATE', 'CP_DB_COLLATE');
+
+define('AUTH_KEY', 'CP_AUTH_KEY');
+define('SECURE_AUTH_KEY', 'CP_SECURE_AUTH_KEY');
+define('LOGGED_IN_KEY', 'CP_LOGGED_IN_KEY');
+define('NONCE_KEY', 'CP_NONCE_KEY');
+define('AUTH_SALT', 'CP_AUTH_SALT');
+define('SECURE_AUTH_SALT', 'CP_SECURE_AUTH_SALT');
+define('LOGGED_IN_SALT', 'CP_LOGGED_IN_SALT');
+define('NONCE_SALT', 'CP_NONCE_SALT');
+
+$table_prefix  = 'CP_DB_TABLE_PREFIX';
+
+define('WP_DEBUG', false);
+
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+require_once(ABSPATH . 'wp-settings.php');
