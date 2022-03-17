f31e809a890e90e54324d5a206c00dfffac3de8f
Marek Sierociński <mareksierocinski@gmail.com>
2022-03-17 18:14:34 +0100

    Whole wp-content should be in /data

--- classicpress/classicpress.sh
+++ classicpress/classicpress.sh
@@ -2,6 +2,21 @@
 
 # ClassicPress Startup Script
 
+WP_CONFIG=/data/wp-config.php
+TMP_WP_CONFIG=/tmp/wp-config.php
+
+
+if [ ! -f "${WP_CONFIG}" ]; then
+    echo "Error: File ${WP_CONFIG} not found"
+    exit 1
+fi
+
+if [ ! -w "${WP_CONFIG}" ]; then
+    echo "Error: File ${WP_CONFIG} found, but not writable. Have you mounted the /data as a volume?"
+    exit 1
+fi
+
+
 function demand_env() {
   if [[ -z "${!1}" ]]; then
     echo "$1 is not set"
@@ -16,52 +31,56 @@ function support_env() {
 }
 
 function random_env() {
-  random_key=`openssl rand -hex 16`
+  random_key=`< /dev/urandom tr -dc "#%+0-;@-Z_a-z~" | head -c32; echo`
   printf -v "$1" "%s" "$random_key"
 }
 
 function store_env() {
-  sed -i "s/$1/${!1}/g" /tmp/wp-config.php
+  sed -i "s/$1/${!1}/g" "${TMP_WP_CONFIG}"
 }
 
 
-echo "Checking environment ..."
-demand_env "CP_DB_NAME"
-demand_env "CP_DB_USER"
-demand_env "CP_DB_PASSWORD"
-demand_env "CP_DB_HOST"
-support_env "CP_DB_CHARSET" "utf8"
-support_env "CP_DB_COLLATE" ""
-support_env "CP_DB_TABLE_PREFIX" "cp_"
+if [ -s "${WP_CONFIG}" ]; then
+  echo "${WP_CONFIG} seems to be already configured. Not recreating."
+else
+  echo "Checking environment ..."
+  demand_env "CP_DB_NAME"
+  demand_env "CP_DB_USER"
+  demand_env "CP_DB_PASSWORD"
+  demand_env "CP_DB_HOST"
+  support_env "CP_DB_CHARSET" "utf8"
+  support_env "CP_DB_COLLATE" ""
+  support_env "CP_DB_TABLE_PREFIX" "cp_"
 
-echo "Generating random strings ..."
-random_env "CP_AUTH_KEY"
-random_env "CP_SECURE_AUTH_KEY"
-random_env "CP_LOGGED_IN_KEY"
-random_env "CP_NONCE_KEY"
-random_env "CP_AUTH_SALT"
-random_env "CP_SECURE_AUTH_SALT"
-random_env "CP_LOGGED_IN_SALT"
-random_env "CP_NONCE_SALT"
+  echo "Generating random strings ..."
+  random_env "CP_AUTH_KEY"
+  random_env "CP_SECURE_AUTH_KEY"
+  random_env "CP_LOGGED_IN_KEY"
+  random_env "CP_NONCE_KEY"
+  random_env "CP_AUTH_SALT"
+  random_env "CP_SECURE_AUTH_SALT"
+  random_env "CP_LOGGED_IN_SALT"
+  random_env "CP_NONCE_SALT"
 
-echo "Preparing wp-config.php ..."
-cp $WWW_DIR/wp-config.template.php /tmp/wp-config.php
-store_env "CP_DB_NAME"
-store_env "CP_DB_USER"
-store_env "CP_DB_PASSWORD"
-store_env "CP_DB_HOST"
-store_env "CP_DB_CHARSET"
-store_env "CP_DB_COLLATE"
-store_env "CP_AUTH_KEY"
-store_env "CP_SECURE_AUTH_KEY"
-store_env "CP_LOGGED_IN_KEY"
-store_env "CP_NONCE_KEY"
-store_env "CP_AUTH_SALT"
-store_env "CP_SECURE_AUTH_SALT"
-store_env "CP_LOGGED_IN_SALT"
-store_env "CP_NONCE_SALT"
-store_env "CP_DB_TABLE_PREFIX"
-mv /tmp/wp-config.php /data/wp-config.php
+  echo "Preparing wp-config.php ..."
+  cp "${WWW_DIR}/../wp-config.template.php" "${TMP_WP_CONFIG}"
+  store_env "CP_DB_NAME"
+  store_env "CP_DB_USER"
+  store_env "CP_DB_PASSWORD"
+  store_env "CP_DB_HOST"
+  store_env "CP_DB_CHARSET"
+  store_env "CP_DB_COLLATE"
+  store_env "CP_AUTH_KEY"
+  store_env "CP_SECURE_AUTH_KEY"
+  store_env "CP_LOGGED_IN_KEY"
+  store_env "CP_NONCE_KEY"
+  store_env "CP_AUTH_SALT"
+  store_env "CP_SECURE_AUTH_SALT"
+  store_env "CP_LOGGED_IN_SALT"
+  store_env "CP_NONCE_SALT"
+  store_env "CP_DB_TABLE_PREFIX"
+  mv "${TMP_WP_CONFIG}" "${WP_CONFIG}"
+fi
 
 echo "Starting Apache in foreground ..."
 # https://github.com/docker-library/php/blob/master/7.4/bullseye/apache/apache2-foreground
